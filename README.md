a153a56a441d8c89e515145f02ffda6075cf2102
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2023-05-24 17:16:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tribute-page
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-5qt9fc)
---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const myNav = document.querySelector('.navigation');
+const myHamburger = document.querySelector('.ham-icon');
+const menuItem = document.querySelectorAll('.menu-item');
+const hamIcon = document.querySelector('.fa-bars');
+
+console.log(menuItem);
+const toggleIcon = function() {
+  if(myNav.classList.contains('responsive')) {
+    hamIcon.classList.remove('fa-bars');
+    hamIcon.classList.add('fa-times');
+
+  } else {
+    hamIcon.classList.add('fa-bars');
+    hamIcon.classList.remove('fa-times');
+  }
+};
+
+myHamburger.addEventListener('click', function () {
+  myNav.classList.toggle('responsive');
+  toggleIcon();
+});
+
+menuItem.forEach(function (e) {
+  e.addEventListener('click', function() {
+    myNav.classList.remove('responsive');
+    toggleIcon();
+  })
+})
